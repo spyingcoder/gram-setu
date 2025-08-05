53dd2323f7b868c15c2d709a7fcb295931d00711
spyingcoder <spyingcoder@gmail.com>
2025-08-05 22:57:33 +0530

    Integrate complaint submission with loading/success state using Status enum

--- lib/features/complaints/cubit/complaint_state.dart
+++ lib/features/complaints/cubit/complaint_state.dart
@@ -4,22 +4,33 @@ class ComplaintState {
   final String title;
   final String description;
   final File? selectedImage;
+  final Status status;
 
   ComplaintState({
     required this.title,
     required this.description,
     required this.selectedImage,
+    required this.status,
   });
+
+  factory ComplaintState.initial() => ComplaintState(
+        title: '',
+        description: '',
+        selectedImage: null,
+        status: Status.initial,
+      );
 
   ComplaintState copyWith({
     String? title,
     String? description,
     File? selectedImage,
+    Status? status,
   }) {
     return ComplaintState(
       title: title ?? this.title,
       description: description ?? this.description,
-      selectedImage: selectedImage,
+      selectedImage: selectedImage ?? this.selectedImage,
+      status: status ?? this.status,
     );
   }
 }
